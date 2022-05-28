93e4930be654a472d7c0fce5e5a4b3098d434cb6
Brett Longworth <blongworth@whoi.edu>
2022-05-28 11:32:21 -0400

    Add type selection

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(shiny)
 library(DT, warn.conflicts = FALSE)
 library(amstools)
 library(ggplot2)
+library(dplyr)
 
 # Define UI
 ui <- shinyUI(
@@ -16,10 +17,11 @@ ui <- shinyUI(
     sidebarLayout(
       sidebarPanel(
         selectInput("magazineSelect",
-                    label = h3("Magazine"),"")#,
-        # radioButtons("type", label = h3("Sample type"),
-        #              choices = list("Standards" = 1, "Blanks" = 2, "All samples" = 3),
-        #              selected = 1),
+                    label = h3("Magazine"),""),
+        radioButtons("type",
+                     label = h3("Sample type"),
+                     choices = list("all")),
+                     # selected = 1),
         # checkboxInput("box", label = "Boxplot?", value = FALSE),
         # checkboxInput("oxi", label = "Use only OX-I primaries?", value = FALSE),
         # checkboxInput("group", label = "Last group only?", value = FALSE)
@@ -43,9 +45,9 @@ ui <- shinyUI(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
-  # Get list of micadas magazines
+  # Get list of micadas magazines most recent first
   sqlOutput<- reactive({
-    amstools::list_magazines()
+    rev(list_magazines())
   })
 
   observe ({
@@ -58,11 +60,26 @@ server <- function(input, output, session) {
     get_magazine(input$magazineSelect)
   })
 
-    output$Plot <- renderPlot({
-      mag_df() |>
+  observe({
+    types <- unique(mag_df()$type)
+    updateRadioButtons(session, "type",
+                       choices = c('all', types))
+  })
+
+  mag_sub_df <- reactive({
+    df <- mag_df()
+    if (input$type != 'all') {
+    df <- df %>%
+      filter(type == input$type)
+    }
+    df
+  })
+
+  output$Plot <- renderPlot({
+    mag_sub_df() %>%
       ggplot(aes(TIMEDAT, RA, color = type)) +
-        geom_point()
-    })
+      geom_point()
+  })
 }
 
 # Run the application
